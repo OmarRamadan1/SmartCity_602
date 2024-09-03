9ab7bf872d338704b7a116288b83d075523af66b
Esraa <esraaaboelkhair8@gmail.com>
2024-09-03 16:02:51 +0300

    updated code

--- light.py
+++ light.py
@@ -2,31 +2,41 @@
 import RPi.GPIO as GPIO
 from gpiozero import LED
 import time
-import paho.mqtt.client as mqtt
-
+#import paho.mqtt.client as mqtt
 
 #MQTT
-Broker_URL = "50c69f01d2b349de8f734066b3b5a261.s1.eu.hivemq.cloud"
-Broker_Port = "8883"
-Topic_IR = "IR"
-Topic_LDR = "LDR"
-client = mqtt.Client()
-client.username_pw_set("User1", "User1234")
-client.tls_set()
-client.connect(broker_url, broker_port)
+#Broker_URL = "50c69f01d2b349de8f734066b3b5a261.s1.eu.hivemq.cloud"
+#Broker_Port = "8883"
+#Topic_IR = "IR"
+#Topic_LDR = "LDR"
+#client = mqtt.Client()
+#client.username_pw_set("User1", "User1234")
+#client.tls_set()
+#client.connect(broker_url, broker_port)
+
 #Hardware Pins
-led_pins = [17, 18, 27, 22, 23, 24]
-ir_sensors = [4, 17, 27,6]
-ldr_pin = 4  
+GPIO.setmode(GPIO.BOARD)
+#led_pins = [35, 37, 36, 38]
+LED_Pin_1 = LED(19)
+LED_Pin_2 = LED(26)
+LED_Pin_3 = LED(16)
+LED_Pin_4 = LED(20)
+#ir_sensors = [11, 13, 15,18]
+IR_Sensor_1 = 11
+IR_Sensor_2 = 13
+IR_Sensor_3 = 15
+IR_Sensor_4 = 16
+pin_to_circuit = 12 
 
 #Initializing IR Sensors 
-for sensor in ir_sensors:
-    GPIO.setup(sensor, GPIO.IN)
+#for sensor in ir_sensors:
+#    GPIO.setup(sensor, GPIO.IN)
+GPIO.setup(IR_Sensor_1,GPIO.IN)
+GPIO.setup(IR_Sensor_2,GPIO.IN)
+GPIO.setup(IR_Sensor_3,GPIO.IN)
+GPIO.setup(IR_Sensor_4,GPIO.IN)
 
-#Initializing LEDs 
-for pin in led_pins:
-    GPIO.setup(pin, GPIO.OUT)
-    GPIO.output(pin, GPIO.LOW)
+
     
 #Getting LDR Value
 def rc_time (pin_to_circuit):
@@ -41,35 +51,47 @@ def rc_time (pin_to_circuit):
         count += 1
 
     return count
-
-#Function Actions based on LDR Value
-def is_night():
-  LDR_Value = rc_time(ldr_pin)
-  client.publish(Topic_LDR,LDR_Value,qos=1)
-  if ldr_value > 2500:
-    return False
-  else:
-    return True
-
-#Function to Control LEDs 
-def control_leds(sensor_index):
-    if is_night():
-        if 0 <= sensor_index < len(led_pins) // 2:
-            GPIO.output(led_pins[sensor_index * 2], GPIO.HIGH)
-            GPIO.output(led_pins[sensor_index * 2 + 1], GPIO.HIGH)
-    else:
-        for pin in led_pins:
-            GPIO.output(pin, GPIO.LOW)
-
-#Operating IR with LDR 
 try:
     while True:
-        for i, pin in enumerate(ir_sensors):
-			IR_Value = GPIO.input(pin)
-			client.publish(Topic_IR,IR_Value,qos=1)
-            if IR_Value == GPIO.LOW:
-                control_leds(i)
-        time.sleep(1)
+            IR_Value_1 = GPIO.input(IR_Sensor_1)
+            IR_Value_2 = GPIO.input(IR_Sensor_2)
+            IR_Value_3 = GPIO.input(IR_Sensor_3)
+            IR_Value_4 = GPIO.input(IR_Sensor_4)
+            LDR_Value = rc_time(pin_to_circuit)
+            #print(LDR_Value)
+#  client.publish(Topic_LDR,LDR_Value,qos=1)
+            if LDR_Value > 2500:
+                if not IR_Value_1:
+                    LED_Pin_1.on()
+                    LED_Pin_2.off()
+                    LED_Pin_3.off()
+                    LED_Pin_4.off()
+                elif not IR_Value_2:
+                    LED_Pin_1.off()
+                    LED_Pin_2.on()
+                    LED_Pin_3.off()
+                    LED_Pin_4.off()
+                elif not IR_Value_3:
+                    LED_Pin_1.off()
+                    LED_Pin_2.off()
+                    LED_Pin_3.on()
+                    LED_Pin_4.off()
+                elif not IR_Value_4:
+                    LED_Pin_1.off()
+                    LED_Pin_2.off()
+                    LED_Pin_3.off()
+                    LED_Pin_4.on()	
+				#return False
+            else:
+                LED_Pin_1.off()
+                LED_Pin_2.off()
+                LED_Pin_3.off()
+                LED_Pin_4.off()
+                #print("Light")
+
+                
+            time.sleep(0.05)                
+#			client.publish(Topic_IR,IR_Value,qos=1)
 
 except KeyboardInterrupt:
     pass
